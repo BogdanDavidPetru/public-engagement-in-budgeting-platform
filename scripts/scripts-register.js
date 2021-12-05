da11238efed45c82aecdc018c10319731a4d088b
Bogdan David Petru <56302334+BogdanDavidPetru@users.noreply.github.com>
2021-12-05 12:59:02 +0200

    Implemented login feature and register page for both individual and institution and also connected home page to register page (#3)
    * first steps for ogin page
    
    * changed login task and added first iteration of register component
    
    * implemented register page for institution and individual and connected home page to register

--- scripts/scripts-register.js
+++ scripts/scripts-register.js
@@ -25,6 +25,15 @@ $(document).ready(function(){
         return false;
     })
 
+    // var $institutionOption=$("#institution-option");
+    // var $institutionForm=$("#institution-input");
+    // var $individualOption=$("#individual-input");
+    // $institutionOption.on("selected",function(){
+    //     alert("pula");
+    //     $individualOption.hide();
+    //     $institutionForm.show();
+    // })
+
     var $projects = $("#projects");
     var $projectOptionList = $("#project-option");
     $projectOptionList.hide();
@@ -40,21 +49,19 @@ $(document).ready(function(){
         }
         return false;
     });
-    $.get( "http://jservice.io/api/random?count=10", showQuestions);
+
+     var $institutionForm=$("[id=institution-input]");
+    var $individualOption=$("[id=individual-input]");
+    $("#person-type").change(function(){
+        var cur_value = $('#person-type option:selected').text();
+        console.log(cur_value);
+        if(cur_value==="Institution"){
+            $individualOption.hide();
+            $institutionForm.show();
+        }
+        if(cur_value==="Individual"){
+            $institutionForm.hide();
+            $individualOption.show();
+        }
+      });
 })
-function showQuestions(data) {
-    $( data ).each ( function () {    
-    $("<li>")
-    // .append( $("<p>").addClass("question").text("Question: " + this.question))
-    .append( $("<span>").addClass("answer").text("Answer: " + this.answer))
-     .append( $("<p>").addClass("value").text("Value: "+this.value))
-     .append($("<button>").addClass("").text("View statistics"))
-    // .append( $("<p>").addClass("createdAt").text("Created at: " + this.created_at))
-    // .append( $("<p>").addClass("categoryTitle").text("Category Title: "+this.category.title))
-    .appendTo("#project-list")
-    // .append( $("<div>").addClass("author_affiliation").text(this.institution))
-    // .append( $("<span>").text(this.email))
-    // .appendTo("#authors-list")
-    });
-    
-} 
